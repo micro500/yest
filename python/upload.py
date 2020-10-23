2672669285701743b11fb8c676493de0c359d540
pjgat09 <pjgat09@gmail.com>
2020-10-22 22:48:25 -0400

    Actual upload handling code

--- python/upload.py
+++ python/upload.py
@@ -1,8 +1,22 @@
 import json
+import random
+import boto3
+from botocore.config import Config
 
 def lambda_handler(event, context):
-    # TODO implement
+    client = boto3.client('ssm')
+
+    images_bucket = client.get_parameter(Name='YestImagesS3Bucket')["Parameter"]["Value"]
+
+    s3Client = boto3.client('s3', region_name='us-west-2', config = Config(s3={'addressing_style': 'virtual'}))
+    id = random.randrange(10000000)
+    filename = str(id) + ".jpg"
+    response = s3Client.generate_presigned_post(images_bucket, filename, Fields=None, Conditions=None, ExpiresIn=3600)
+    
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps(response),
+        'headers': {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, DELETE'}
     }
